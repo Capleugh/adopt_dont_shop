2fd25a22140e0f1e130183c833cb4471cd0ed240
Carleigh Crockett <capleugh@gmail.com>
2019-11-20 16:04:47 -0700

    Create new db and migration to include more shelter info

--- spec/features/shelters/user_can_see_shelters_spec.rb
+++ spec/features/shelters/user_can_see_shelters_spec.rb
@@ -2,8 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "shelters index page", type: :feature do
   it "can see the name all of each shelter" do
-    shelter_1 = Shelter.create(name: "Forever Home Finder")
-    shelter_2 = Shelter.create(name: "Rescuers Up Over")
+    shelter_1 = Shelter.create(name: "Forever Home Finder",
+                               address: "246 Glenwood Dr",
+                               city: "Boulder",
+                               state: "CO",
+                               zip: "80304")
+    shelter_2 = Shelter.create(name: "Rescuers Up Over",
+                               address: "246 Glenwood Dr",
+                               city: "Boulder",
+                               state: "CO",
+                               zip: "80304")
 
     visit "/shelters"
 
