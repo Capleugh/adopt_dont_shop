24a471ac1d46aaceaf7bf3bcb8e5079cfdc7f04c
Carleigh Crockett <capleugh@gmail.com>
2019-11-20 20:03:00 -0700

    Drop and remigrate table

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,4 @@ ActiveRecord::Schema.define(version: 20191120223552) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "shelters", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.string "city"
-    t.string "state"
-    t.string "zip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
