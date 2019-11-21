f10131dc544412df7e4a91b49fe43121a7fd5f88
Carleigh Crockett <capleugh@gmail.com>
2019-11-21 15:48:47 -0700

    Add link test and route for new shelter

--- spec/features/shelters/user_can_fill_out_form_to_create_new_shelter_spec.rb
+++ spec/features/shelters/user_can_fill_out_form_to_create_new_shelter_spec.rb
@@ -1,9 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe "new page", type: :feature do
-  it "can display a form to add a new shelter" do
-    
+RSpec.describe "New Shelter", type: :feature do
+  describe "As a visitor" do
+    describe "when I visit the shelter index page" do
+      describe "then I see a link to create a new shelter" do
+        describe "when I click this link" do
+          it "I am taken to /shelters/new where I see a for for a new shelter" do
+            visit '/shelters'
 
+            click_link 'New Shelter'
 
+            expect(current_path).to eq('/shelters/new')
+          end
+        end
+      end
+    end
   end
 end
+
+# Then I see a link to create a new Shelter
+# When I click this link
+# Then I am taken to '/shelters/new' where I  see a form for a new shelter
+#
+# When I fill out the form with a new shelter's:
+# - name
+# - address
+# - city
+# - state
+# - zip
+# And I click the button to submit the form
+# Then a `POST` request is sent to '/shelters',
+# a new shelter is created,
+# and I am redirected to the Shelter Index page where I see the new Shelter listed.
