e589b263ec13cdaf7d106fc87f796a50816decd4
Carleigh Crockett <capleugh@gmail.com>
2019-11-23 20:00:29 -0700

    Create pet model and test

---
+++ spec/features/pets/new_spec.rb
@@ -0,0 +1,10 @@
+# require 'rails_helper'
+#
+# RSpec.describe "As a visitor" do
+#   describe "when I visit the pets index page" do
+#     it "I see each pet in the system and all of their info" do
+#
+#
+#     end
+#   end
+# end
