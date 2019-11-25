9ced3bd6bc97c53ceae19edac808fd19b40d22fe
Carleigh Crockett <capleugh@gmail.com>
2019-11-24 19:07:59 -0700

    Update index view to include a shelters path to see pets that belong to a shelter

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,5 +1,14 @@
 class PetsController < ApplicationController
   def index
-    @pets = Pet.all
+    # require "pry"; binding.pry
+    if params[:shelter_id]
+      @shelter = Shelter.find(params[:shelter_id])
+      @pets = @shelter.pets
+    else
+      @pets = Pet.all
+    # come back and revisit this to make sure you fully comprehend it
+    end
+
+
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,16 @@ shelter_2 = Shelter.create(name: "Forever Home Finder",
                state: "CO",
                zip: "80303")
 
-shelter_1.pets.create(image: 'https://i.ytimg.com/vi/7NYaGOyJiCY/maxresdefault.jpg',
+shelter_2.pets.create(image: 'https://i.ytimg.com/vi/7NYaGOyJiCY/maxresdefault.jpg',
                       name: 'Stannislaus',
                       approximate_age: 75,
                       sex: 'male')
+
+shelter_1.pets.create(image: 'https://i.imgur.com/RuDsNGNh.jpg',
+                      name: 'DJ Ravioli',
+                      approximate_age: 7,
+                      sex: 'male')
+shelter_1.pets.create(image: 'https://vetstreet.brightspotcdn.com/dims4/default/02bd838/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2Fa3%2F767b00a33511e087a80050568d634f%2Ffile%2FSphynx-4-645mk062211.jpg',
+                      name: 'Ruth Bader Ginspurrg',
+                      approximate_age: 12,
+                      sex: 'female')
