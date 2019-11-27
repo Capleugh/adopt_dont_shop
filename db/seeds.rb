20a6d1daf1b2a27170cfc634c64a57e88790c447
Carleigh Crockett <capleugh@gmail.com>
2019-11-26 18:35:39 -0700

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,7 +32,7 @@ shelter_1.pets.create(image: 'https://i.imgur.com/RuDsNGNh.jpg',
                       description: "This cross-eyed catto loves bath time and rap time.",
                       approximate_age: 7,
                       sex: 'male',
-                      status: 'pending adoption')
+                      status: 'adoptable')
 shelter_1.pets.create(image: 'https://vetstreet.brightspotcdn.com/dims4/default/02bd838/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2Fa3%2F767b00a33511e087a80050568d634f%2Ffile%2FSphynx-4-645mk062211.jpg',
                       name: 'Ruth Bader Ginspurrg',
                       description: 'Loves sweaters, impartiality, and serving her country.',
